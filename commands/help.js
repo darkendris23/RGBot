98466ad80d005c64555e712a7a0905e101d103c3
Glitch (r-g-bot) <none>
2020-03-01 15:16:48 +0000

    🐥💷 Updated with Glitch

---
+++ commands/help.js
@@ -0,0 +1,5 @@
+exports.run = async (client, message, args) => {
+  if (!args.length) {
+    return message.channel.send("hi");
+  }
+};
--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,14 @@
-exports.run = async(client, message, args) => {
-message.channel.send("Pong :) ")
+exports.run = async (client, message, args) => {
+  if (!message.mentions.users.size) {
+    message.reply("Pong");
+  } else {
+    const taggedUser = message.mentions.users.first();
 
-}
+    message.channel.send(
+      "Hey, " +
+        `${taggedUser.username}` +
+        " you were pinged by " +
+        `${message.author}`
+    );
+  }
+};
