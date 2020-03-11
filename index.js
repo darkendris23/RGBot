878b70ebc20d7ca2b0ded1dee083f12aa9b4ae6c
Glitch (r-g-bot) <none>
2020-03-11 03:35:20 +0000

    🍇👆 Updated with Glitch

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const Discord = require("discord.js");
+
 const client = new Discord.Client();
+const config = require("./config.json");
+client.config = config;
+
+const Enmap = require("enmap");
 const fs = require("fs");
 require(__dirname + "/app/keepAlive.js");
 
@@ -8,29 +13,30 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.events = new Discord.Collection();
 
+//Events "handler"
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+
 //Commands
+client.commands = new Enmap();
 
 fs.readdir("./commands/", (err, files) => {
-  if (err) return console.log(err);
+  if (err) return console.error(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
-    console.log("Successfully loaded " + file);
     let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
   });
 });
 
-//Events "handler"
-fs.readdir("./events/", (err, files) => {
-  if (err) console.log(err);
-  files.forEach(file => {
-    let eventFunc = require(`./events/${file}`);
-    console.log("Successfully loaded " + file);
-    let eventName = file.split(".")[0];
-    client.on(eventName, (...args) => eventFunc.run(client, ...args));
-  });
-});
 
-client.on("ready", () => console.log("Online!"));
 client.login(process.env.TOKEN);
