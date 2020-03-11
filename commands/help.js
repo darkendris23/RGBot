7dd73e92e7e4cb4989746b949a8b301ccdddc7a0
Glitch (r-g-bot) <none>
2020-03-11 23:19:15 +0000

    🗺👼 Updated with Glitch

--- commands/help.js
+++ commands/help.js
@@ -2,3 +2,16 @@ module.exports.run = (client, message, args) => {
   if (!args.length) return message.channel.send("Hi");
   
 };
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["h", "help"]
+};
+
+module.exports.help = {
+  name: "help",
+  category: "System",
+  description: "Displays all the available commands for your permission level.",
+  usage: "help [command]"
+};
